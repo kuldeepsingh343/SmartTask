3e07d066afada59a7ea4865f6a3fb56f8e6aed47
Kuldeep Singh <kuldeepsingh343@gmail.com>
2026-05-26 22:53:16 -0500

    Fix: Set Info.plist auto-generation, update bundle identifier, and remove missing preview asset paths

--- SmartTask/generate_project.rb
+++ SmartTask/generate_project.rb
@@ -5,14 +5,14 @@ project_path = "#{project_name}.xcodeproj"
 project = Xcodeproj::Project.new(project_path)
 
 target = project.new_target(:application, project_name, :ios, '15.0')
+target.build_configuration_list.set_setting('GENERATE_INFOPLIST_FILE', 'YES')
 target.build_configuration_list.set_setting('INFOPLIST_KEY_UIApplicationSceneManifest_Generation', 'YES')
 target.build_configuration_list.set_setting('INFOPLIST_KEY_UIApplicationSupportsIndirectInputEvents', 'YES')
 target.build_configuration_list.set_setting('INFOPLIST_KEY_UILaunchScreen_Generation', 'YES')
-target.build_configuration_list.set_setting('PRODUCT_BUNDLE_IDENTIFIER', 'com.antigravity.SmartTask')
+target.build_configuration_list.set_setting('PRODUCT_BUNDLE_IDENTIFIER', 'com.kuldeepsingh.SmartTask')
 target.build_configuration_list.set_setting('SWIFT_VERSION', '5.0')
 target.build_configuration_list.set_setting('MARKETING_VERSION', '1.0')
 target.build_configuration_list.set_setting('CURRENT_PROJECT_VERSION', '1')
-target.build_configuration_list.set_setting('DEVELOPMENT_ASSET_PATHS', '"SmartTask/Preview Content"')
 target.build_configuration_list.set_setting('ENABLE_PREVIEWS', 'YES')
 
 
